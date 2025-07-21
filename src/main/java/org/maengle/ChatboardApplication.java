fc3b7e29088b98b11489a736f45b33589df4cca4
KimMarinet <kiana507680@gmail.com>
2025-07-21 12:51:32 +0900

    공통 ver.1

---
+++ src/main/java/org/maengle/ChatboardApplication.java
@@ -0,0 +1,13 @@
+package org.maengle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatboardApplication.class, args);
+	}
+
+}
