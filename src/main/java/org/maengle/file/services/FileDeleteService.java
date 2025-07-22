084dc4af1be7bcc891d058b9aa42aee83ba89ed5
juyonghyeon <jyh928578@gmail.com>
2025-07-22 15:52:17 +0900

    삭제 처리 관련 주석 추가

--- src/main/java/org/maengle/file/services/FileDeleteService.java
+++ src/main/java/org/maengle/file/services/FileDeleteService.java
@@ -18,7 +18,7 @@ public class FileDeleteService {
     private final FileInfoService infoService;
     private final FileInfoRepository repository;
 
-    // 파일 등록번호(seq)로 삭제 처리
+    // 파일 등록번호(seq)로 삭제 처리 (눈에 안보이는 기능)
     public FileInfo deleteProcess(Long seq) {
         FileInfo item = infoService.get(seq);
 
@@ -40,6 +40,7 @@ public class FileDeleteService {
     }
 
 
+    // 파일 삭제 처리 메서드(눈에 보이는 기능)
     public List<FileInfo> process(String gid, String location) {
         List<FileInfo> items = infoService.getList(gid, location, FileStatus.ALL);
         List<FileInfo> deletedItems = new ArrayList<>();
@@ -53,6 +54,7 @@ public class FileDeleteService {
         return deletedItems;
     }
 
+    // 게시글에 첨부된 파일 삭제 (location 지정 없이 gid(그룹)으로 삭제)
     public List<FileInfo> process(String gid) {
         return process(gid, null);
     }
