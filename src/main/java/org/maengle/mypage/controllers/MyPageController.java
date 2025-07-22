547adb44196b37b8897c46baa1c71bf810117e30
yesung <jys_troy@naver.com>
2025-07-22 17:34:21 +0900

    mypage 컨트롤러 미사용 코드 주석처리 및 html 아주 약간수정

--- src/main/java/org/maengle/mypage/controllers/MyPageController.java
+++ src/main/java/org/maengle/mypage/controllers/MyPageController.java
@@ -1,7 +1,6 @@
 package org.maengle.mypage.controllers;
 
 import lombok.RequiredArgsConstructor;
-import org.maengle.global.libs.Utils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/mypage")
 @RequiredArgsConstructor
 public class MyPageController {
-    private final Utils utils;
+    // 아래 index 사용하기 위한 tpl 선언이 안되어 있어서 미리 지정만 했습니다.
+//    private final Utils utils;
 
     @GetMapping
     public String index() {
