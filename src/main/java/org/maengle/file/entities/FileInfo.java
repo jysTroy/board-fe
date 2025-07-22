f150da78bfbb7ac18582fa86e68e8dc7dda473b1
juyonghyeon <jyh928578@gmail.com>
2025-07-22 11:25:17 +0900

    파일 작업 기본 소스

---
+++ src/main/java/org/maengle/file/entities/FileInfo.java
@@ -0,0 +1,57 @@
+package org.maengle.file.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.maengle.global.entities.BaseEntity;
+import org.springframework.data.annotation.CreatedBy;
+
+@Data
+@Entity
+@Table(indexes = {
+        @Index(name="idx_fileinfo_gid1", columnList = "gid,createdAt"),
+        @Index(name="idx_fileinfo_gid2", columnList = "gid,done,createdAt"),
+        @Index(name="idx_fileinfo_location1", columnList = "gid,location,createdAt"),
+        @Index(name="idx_fileinfo_location2", columnList = "gid,location,done,createdAt")
+})
+public class FileInfo extends BaseEntity {
+
+    @Id
+    @GeneratedValue
+    private Long seq;
+
+    @Column(length = 45, nullable = false)
+    private String gid;
+
+    @Column(length = 45)
+    private String location;
+
+    @Column(nullable = false, length = 150)
+    private String fileName;
+
+    @Column(length = 60)
+    private String extension;
+
+    @Column(length = 100)
+    private String contentType;
+
+    @CreatedBy
+    private String createdBy;
+
+    private boolean done;
+
+    @Transient
+    private String filePath;
+
+    @Transient
+    private String fileUrl;
+
+    @Transient
+    private String thumbBaseUrl;
+
+    @Transient
+    private String thumbBasePath;
+
+    @Transient
+    private boolean image;
+
+}
---
+++ src/main/java/org/maengle/file/repositories/FileInfoRepository.java
@@ -0,0 +1,8 @@
+package org.maengle.file.repositories;
+
+import org.maengle.file.entities.FileInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+
+public interface FileInfoRepository extends JpaRepository<FileInfo, Long>, QuerydslPredicateExecutor<FileInfo> {
+}
