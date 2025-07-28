f629364fab77149f68d2c7afe8a107a63e4ce557
yesung <jys_troy@naver.com>
2025-07-28 15:06:31 +0900

    병합용

--- src/main/java/org/maengle/admin/board/controllers/AdminBoardController.java
+++ src/main/java/org/maengle/admin/board/controllers/AdminBoardController.java
@@ -1,7 +1,10 @@
 package org.maengle.admin.board.controllers;
 
 import org.maengle.admin.global.controllers.CommonController;
+import org.maengle.global.search.CommonSearch;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,4 +19,13 @@ public class AdminBoardController extends CommonController {
     }
 
     // 게시판 목록
+    @GetMapping({"", "/list"})
+    public String list(@ModelAttribute CommonSearch search, Model model) {
+
+        // 보드 인포서비스 만드는 공사중
+//        ListData<Board> data =
+
+        return "admin/board/list";
+    }
+
 }
--- src/main/java/org/maengle/board/services/configs/BoardConfigInfoService.java
+++ src/main/java/org/maengle/board/services/configs/BoardConfigInfoService.java
@@ -1,4 +1,39 @@
 package org.maengle.board.services.configs;
 
+import lombok.RequiredArgsConstructor;
+import org.maengle.admin.board.controllers.RequestBoard;
+import org.maengle.board.entities.Board;
+import org.maengle.board.exceptions.BoardNotFoundException;
+import org.maengle.board.repositories.BoardRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class BoardConfigInfoService {
+
+    private final ModelMapper mapper;
+    private final BoardRepository repository;
+
+    // 게시판 설정 한 개 조회
+    public Board get(String bid) {
+        Board item = repository.findById(bid).orElseThrow(BoardNotFoundException::new);
+
+        addInfo(item); // 추가 정보 공통 처리
+
+        return item;
+    }
+
+    // 게시판 설정 수정시 필요한 객체로 조회
+    public RequestBoard getForm(String bid)
+    {
+        Board board = get(bid);
+
+        return mapper.map(board, RequestBoard.class);
+    }
+
+    // 게시판 설정 추가 정보 가공 처리
+    private void addInfo(Board item) {
+
+    }
 }
