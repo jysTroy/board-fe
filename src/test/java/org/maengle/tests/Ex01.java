e450b20b32724cc81141940dd4e99fc0bb9aee23
yesung <jys_troy@naver.com>
2025-08-06 13:12:34 +0900

    .

---
+++ src/test/java/org/maengle/tests/Ex01.java
@@ -0,0 +1,29 @@
+package org.maengle.tests;
+
+import org.junit.jupiter.api.Test;
+import org.maengle.testutils.MockMember;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class Ex01 {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+//    @WithMockUser(authorities = "ADMIN")
+//    @WithUserDetails(value="user01@test.org")
+    @MockMember
+    void test1() throws Exception {
+        mockMvc.perform(get("/mypage"))
+                .andDo(print());
+    }
+}
